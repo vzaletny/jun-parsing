b5920cf4ebb17bbb4a509e60594d022c210a9ffd
vzaletny <vzaletny@gmail.com>
2017-09-11 01:25:15 +0700

    First release

--- jun-parsing.py
+++ jun-parsing.py
@@ -12,21 +12,31 @@ interface_list = []
 regex_des = re.compile(r'^set interfaces +(?P<interface>\S+).*description +(?P<description>\S+)')
 regex_mode = re.compile(r'^set interfaces +(?P<interface>\S+).+ethernet-switching +interface-mode +(?P<mode>\S+)')
 regex_vlans = re.compile(r'^set interfaces +(?P<interface>\S+).+ethernet-switching +vlan +members +(?P<vlan>[\d+-].+)')
+regex_irb = re.compile(r'^set interfaces +(?P<interface>\S+) +unit (?P<unit>\d+).*description +(?P<description>\S+)')
+regex_irb_ip = re.compile(r'^set interfaces +(?P<interface>\S+) +unit (?P<unit>\d+).*address +(?P<ipaddress>\S+)')
+
 last_interface = ''
 interface_desc = None
+last_unit = '0'
+ipaddress = ''
 
 with open('qfx.txt', mode='r') as f:
     for line in f:
         if 'description' in line:
             match = regex_des.search(line)
+            if 'irb' in line:
+                match = regex_irb.search(line)
+                last_unit = match.group('unit')
             if match:
-                if match.group('interface') != last_interface:
+                if match.group('interface') != last_interface or last_unit != '0':
                     if last_interface and interface_desc:
                         interface_list.append(interface_desc)
                     last_interface = match.group('interface')
                     interface_desc = {'interface': match.group('interface'),
                                       'description': match.group('description'),
-                                      'vlans': []}
+                                      'vlans': [],
+                                      'unit': last_unit,
+                                      'ipaddress': ipaddress}
         elif 'ethernet-switching interface-mode' in line:
             match = regex_mode.search(line)
             if match:
@@ -37,6 +47,13 @@ with open('qfx.txt', mode='r') as f:
             if match:
                 if match.group('interface') == last_interface:
                     interface_desc['vlans'].append(match.group('vlan'))
+        elif 'family inet address' in line:
+            match = regex_irb_ip.search(line)
+            if match:
+                if match.group('unit') == last_unit:
+                    interface_desc['ipaddress'] = match.group('ipaddress')
+                # print(match.group('interface'), match.group('unit'), match.group('ipaddress'))
+
 print(tabulate(interface_list, headers='keys', tablefmt='grid'))
 # pprint(interface_list)
 
