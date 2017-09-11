dcef040c5cdbbcdf4866b7d88b8c4f6775b58afd
vzaletny <31381624+vzaletny@users.noreply.github.com>
2017-09-11 17:23:36 +0700

    Second Release 0.2

--- jun-parsing.py
+++ jun-parsing.py
@@ -3,7 +3,10 @@
 import json
 import re
 from tabulate import tabulate
-from pprint import pprint
+# import csv
+# from pprint import pprint
+import unicodecsv as csv
+
 
 interface = ['interface', 'description', 'mode', 'vlans']
 interface_list = []
@@ -13,12 +16,12 @@ regex_des = re.compile(r'^set interfaces +(?P<interface>\S+).*description +(?P<d
 regex_mode = re.compile(r'^set interfaces +(?P<interface>\S+).+ethernet-switching +interface-mode +(?P<mode>\S+)')
 regex_vlans = re.compile(r'^set interfaces +(?P<interface>\S+).+ethernet-switching +vlan +members +(?P<vlan>[\d+-].+)')
 regex_irb = re.compile(r'^set interfaces +(?P<interface>\S+) +unit (?P<unit>\d+).*description +(?P<description>\S+)')
-regex_irb_ip = re.compile(r'^set interfaces +(?P<interface>\S+) +unit (?P<unit>\d+).*address +(?P<ipaddress>\S+)')
+regex_irb_ip = re.compile(r'^set interfaces +(?P<interface>\S+) +unit (?P<unit>\d+).*address +(?P<ip>[\d.]+/\d+)')
 
 last_interface = ''
 interface_desc = None
 last_unit = '0'
-ipaddress = ''
+ip = ''
 
 with open('qfx.txt', mode='r') as f:
     for line in f:
@@ -34,9 +37,9 @@ with open('qfx.txt', mode='r') as f:
                     last_interface = match.group('interface')
                     interface_desc = {'interface': match.group('interface'),
                                       'description': match.group('description'),
-                                      'vlans': [],
+                                      'vlans': '',
                                       'unit': last_unit,
-                                      'ipaddress': ipaddress}
+                                      'ip': ip}
         elif 'ethernet-switching interface-mode' in line:
             match = regex_mode.search(line)
             if match:
@@ -46,16 +49,24 @@ with open('qfx.txt', mode='r') as f:
             match = regex_vlans.search(line)
             if match:
                 if match.group('interface') == last_interface:
-                    interface_desc['vlans'].append(match.group('vlan'))
+                    # interface_desc['vlans'].append(match.group('vlan'))
+                    if not interface_desc['vlans']:
+                        interface_desc['vlans'] = match.group('vlan')
+                    else:
+                        interface_desc['vlans'] += (',' + match.group('vlan'))
         elif 'family inet address' in line:
             match = regex_irb_ip.search(line)
             if match:
                 if match.group('unit') == last_unit and match.group('interface') == last_interface:
-                    interface_desc['ipaddress'] = match.group('ipaddress')
-                # print(match.group('interface'), match.group('unit'), match.group('ipaddress'))
+                    interface_desc['ip'] = match.group('ip')
+                # print(match.group('interface'), match.group('unit'), match.group('ip'))
 
 print(tabulate(interface_list, headers='keys', tablefmt='grid'))
-# pprint(interface_list)
 
 with open('juniper_qfx.json', 'w') as f:
     json.dump(interface_list, f, sort_keys=True, indent=4)
+
+with open('juniper_qfx.csv', 'wb') as f:
+    writer = csv.DictWriter(f, quoting=csv.QUOTE_NONNUMERIC, fieldnames=interface_list[0], delimiter=';')
+    writer.writeheader()
+    writer.writerows(interface_list)
